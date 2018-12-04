6c5bee8f1cd93a705299a81b2271e250a7cd1e66
DeekshithaReddy <deekshithareddy365@gmail.com>
2018-12-04 07:33:26 -0800

    searchclickupdated

--- kafka/services/searchclick.js
+++ kafka/services/searchclick.js
@@ -5,25 +5,10 @@ function handle_request(msg, callback){
     console.log("Inside Search click at Backend")
     let data = msg;
     console.log("Data :", data);
-    Jobs.find({ jobid: msg.jobid})
-    .then(jobdetails=>{
-        console.log("jobdetails",jobdetails);    
-        if( jobdetails.noofclicks==='undefined')
-        {
-                    jobdetails.noofclicks=1;
-                    counter=jobdetails.noofclicks;
-                    console.log("i am in the if block",jobdetails.noofclicks);
-        }
-        else
-        {
-            jobdetails.noofclicks++;
-            counter=jobdetails.noofclicks;
-            console.log("noofclicks updated in else increemnt",jobdetails.noofclicks);
-        console.log("noofclicks got updated",jobdetails.noofclicks);
-        }
         Jobs.findOneAndUpdate({jobid: msg.jobid},{
-            $set: {
-                noofclicks: counter
+            $inc: {
+                noofclicks: 1,
+                noofviewsnotapplied: 1
             }},
             {upsert: true, new: true}).then(jobdetailsupdated=>{
                 console.log("Updtaed clicks successfully", jobdetailsupdated)
@@ -32,11 +17,7 @@ function handle_request(msg, callback){
             .catch(err =>{
                 console.log("jobdetails updtaed err", err)
                 callback(err,null);
-            })})
-    .catch(err =>{
-        console.log("no of clicks err ", err)
-        callback(err,null);
-    })
-}
+            })
+} 
 
 exports.handle_request = handle_request;
